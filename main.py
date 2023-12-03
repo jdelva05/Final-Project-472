bdd1af7449fb51a1d0fe3af28aa0a04e0ca49979
Joshua Delva <132689188+redbolt101@users.noreply.github.com>
2023-12-03 18:07:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ from tkinter import messagebox
 import random
 import threading
 import time
+from datetime import datetime
 
 def create_fall_detection_gui():
     # Create the main window
     root = tk.Tk()
     root.title("Fall Detection System")
 
+    # List to keep track of fall events
+    fall_log = []
+
     # Function to show an alert for fall detection
     def show_fall_alert():
-        messagebox.showwarning("Fall Detected", "A fall has been detected! Immediate assistance is required.")
+        fall_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        messagebox.showwarning("Fall Detected", f"A fall has been detected at {fall_time}! Immediate assistance is required.")
+        fall_log.append(fall_time)
+        # For debugging, you can print the log to the console
+        print(fall_log)
 
     # Function to simulate continuous fall monitoring (a background process)
     def monitor_falls():
